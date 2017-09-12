021311aa4b03f2f39b5494a8b0e85ae43510ac90
Sam <sam@company.com>
2017-09-12 20:43:20 +0100

    fix travis push to docker6

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -2,11 +2,15 @@
 echo $DOCKER_EMAIL
 echo $DOCKER_USER
 echo $DOCKER_PASS
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
+docker login -u samww -p dcatp4dock
+#docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+#if [ "$TRAVIS_BRANCH" = "master" ]; then
+#    TAG="latest"
+#else
+#    TAG="$TRAVIS_BRANCH"
+#fi
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+#docker build -f Dockerfile -t samww/cicd-buzzsw
 docker push $TRAVIS_REPO_SLUG
+#docker push samww/cicd-buzzsw
